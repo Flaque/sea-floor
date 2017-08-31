b848de82660192ea2f5e44e239c34bf76aca7928
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-08-31 09:34:40 -0700

    Remove TODO; refactor tests

--- src/compiler/__test__/main.test.js
+++ src/compiler/__test__/main.test.js
@@ -4,27 +4,21 @@ const compiler = require("../index.js");
 // and not just some weird timeout issue.
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
+function testCompiler(type, filename) {
+  expect.assertions(1);
+  return expect(compiler[type](filename)).resolves.toBeDefined();
+}
+
 describe("compiler", () => {
   it("can compile an example main with no errors", () => {
-    expect.assertions(1);
-    return expect(
-      compiler.main("examples/counter/main.js")
-    ).resolves.toBeDefined();
+    return testCompiler("main", "examples/counter/main.js");
   });
 
   it("can compile a renderer client file with no errors", () => {
-    expect.assertions(1);
-    return expect(
-      compiler.renderer("src/client/index.js")
-    ).resolves.toBeDefined();
+    return testCompiler("renderer", "src/client/index.js");
   });
 
   it("can compile a component file with no errors", () => {
-    expect.assertions(1);
-    return expect(
-      compiler.component("examples/counter/components/App.js")
-    ).resolves.toBeDefined();
+    return testCompiler("component", "examples/counter/components/App.js");
   });
-
-  // TODO Create tests for compiler.renderer and compiler.component
 });
